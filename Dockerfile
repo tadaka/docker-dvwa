5ba2297e7433f85002923bc2aea867dbd2616f76
Cory Sabol <sible777@gmail.com>
2017-02-09 18:46:42 -0500

    script now runs everything as planned

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,33 @@
-from ubuntu
+FROM tutum/lamp:latest
 
-# update the sources
-RUN apt-get update
-# install the stuff needed to run dvwa
-RUN apt-get -y install apache2 mysql-server php5 php5-mysql php5-gd
+MAINTAINER Cory Sabol <corygrandhamsabol@gmail.com>
 
-# set the working dir
-WORKDIR /var/www/html/
-# Bundle the app
-COPY ./dvwa/* /var/www/html/
+# update sources and install php5
+RUN rm -rf /app && \
+    apt-get update && \
+    apt-get install -y wget php5-gd unzip && \
+    rm -rf /var/lib/apt/lists/*
+
+#WORKDIR /var/www/html/
+
+# get DVWA
+RUN wget https://github.com/ethicalhack3r/DVWA/archive/master.zip && \
+    unzip master.zip && \
+    mkdir app && \
+    cp -r DVWA-master/* /app && \
+    rm -rf DVWA-master DVWA-master.zip && \
+    chmod +777 ./app/hackable/uploads/ && \
+    chmod +777 ./app/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt && \
+    sed -i 's/allow_url_include = Off/allow_url_include = On/g' /etc/php5/apache2/php.ini && \
+    sed -i "s/$_DVWA[ 'recaptcha_private_key' ] = ''/$_DVWA[ 'recaptcha_private_key' ] = 'TaQ185RFuWM'/g" /app/config/config.inc.php && \
+    sed -i "s/$_DVWA[ 'recaptcha_public_key' ] = ''/$_DVWA[ 'recaptcha_public_key' ] = 'TaQ185RFuWM'/g" /app/config/config.inc.php && \
+    sed -i 's/FileInfo/All/g' /etc/apache2/sites-available/000-default.conf && \
+    sed -i 's/root/admin/g' /app/config/config.inc.php && \
+    echo "sed -i \"s/p@ssw0rd/\$PASS/g\" /app/config/config.inc.php" >> /create_mysql_admin_user.sh && \
+    echo 'session.save_path = "/tmp"' >> /etc/php5/apache2/php.ini
+
+RUN ls -a 
+
+EXPOSE 80 3306
+
+CMD ["/run.sh"]
--- init.sh
+++ init.sh
@@ -1,9 +1,75 @@
 #!/bin/bash
+# uncg DNS = 152.13.18.154
 
 # get DVWA
+USAGE="init.sh [-h] [-r] [-d ipv4addr] [-b]
+          -h : Display this help
+          -r : run the docker image
+          -d ipv4addr : set the 
+          -b : force a rebuild
+      "
 DVWA_URL=https://github.com/ethicalhack3r/DVWA/archive/master.zip
-wget $DVWA_URL 
+IMAGE="dvwa"
+TAG="hack"
+DOCKER_BUILD="docker build -t $IMAGE:$TAG ."
+DOCKER_RUN="docker run -p 8080:80 -p 3306:3306 $IMAGE:$TAG"
+RUN=NO
+CLEANUP=NO
+REBUILD=NO
 
-# unzip DVWA
-unzip master.zip
-mv DVWA-master/ dvwa/
+# use getopts to handle the args
+while getopts "hrd:b" opt; do
+  case $opt in
+    h) 
+      echo "$USAGE" >&2
+      ;;
+    r)
+      # run the image
+      RUN=YES >&2
+      ;;
+    d)
+      # do some hacky stuff...
+      # modify /etc/default/docker
+      # must be root
+      if [ "$EUID" -ne 0 ]
+      then echo "This option must be run as root." >&2
+        exit 1
+      fi
+      DNS=$OPTARG
+      # should check if the file exists. too lazy for now
+      cp /etc/default/docker /etc/default/docker_old 
+      echo "DOCKER_OPTS=\"--dns $DNS\"" > /etc/default/docker
+      echo "restarting docker service..."
+      sleep 2
+      systemctl restart docker.service
+      CLEANUP=YES
+      ;;        
+    b)
+      echo "Forcing rebuild of container..."
+      REBUILD=YES
+      ;;
+    \?)
+      echo "$USAGE" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# check if the container is already built
+if [[ "$(docker images -q $IMAGE:$TAG 2> /dev/null)" == "" ]] || [[ $REBUILD == YES ]]; then
+  # build the image
+  eval $DOCKER_BUILD 
+fi
+
+if [[ $RUN == YES ]]; then
+  eval $DOCKER_RUN
+fi
+
+if [[ $CLEANUP == YES ]]; then
+  echo "restoring docker settings"
+  cat /etc/default/docker_old > /etc/default/docker
+  echo "default restored"
+  cat /etc/default/docker
+  echo "restarting docker daemon"
+  systemctl restart docker.service
+fi
